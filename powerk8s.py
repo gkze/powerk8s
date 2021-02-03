594e8846b05e3edb2c935574e4b7111b4baad173
George Kontridze <george.kontridze@gmail.com>
2021-02-02 16:44:46 -0800

    Use multilint; clean up

--- powerk8s.py
+++ powerk8s.py
@@ -1,3 +1,10 @@
+"""Powerk8s - a Powerline plugin for Kubernetes information.
+
+Powerk8s reads the local $KUBECNOFIG and displays specified information, such
+as:
+- Current context
+- Namespace
+"""
 from __future__ import annotations
 
 from dataclasses import asdict, dataclass, field
@@ -16,13 +23,17 @@ KUBERNETES_LOGO: str = "\U00002388 "
 
 
 class SegmentArg(Enum):
+    """All possible Powerline segment argument types for Powerk8s."""
+
     SHOW_KUBERNETES_LOGO: str = "show_kube_logo"
     SHOW_CLUSTER: str = "show_cluster"
     SHOW_NAMESPACE: str = "show_namespace"
     SHOW_DEFAULT_NAMESPACE: str = "show_default_namespace"
 
 
-class HighlightGroup:
+class HighlightGroup(Enum):
+    """All possible highlight groups for Powerk8s."""
+
     KUBERNETES_CLUSTER_ALERT: str = "kubernetes_cluster:alert"
     KUBERNETES_CLUSTER: str = "kubernetes_cluster"
     KUBERNETES_DIVIDER: str = "kubernetes:divider"
@@ -32,45 +43,52 @@ class HighlightGroup:
 
 @dataclass
 class SegmentData:
+    """Encapsulates data for a Powerk8s segment."""
+
+    # pylint: disable=unsubscriptable-object
     contents: Optional[str]
     highlight_groups: Sequence[str]
+    # pylint: disable=unsubscriptable-object
     divider_highlight_group: Optional[str] = field(default="")
 
 
 def get_kubernetes_logo(color: str) -> SegmentData:
-    return SegmentData(KUBERNETES_LOGO, [color], HighlightGroup.KUBERNETES_DIVIDER)
+    """Get the Kubernetes logo (it is hardcoded right now)."""
+    return SegmentData(
+        KUBERNETES_LOGO, [color], HighlightGroup.KUBERNETES_DIVIDER.value
+    )
 
 
 def get_segment_args(**kwargs: Any) -> Mapping[SegmentArg, Any]:
+    """Get the arguments for a Powerk8s segment."""
     return {sa: kwargs.get(sa.value, None) for sa in SegmentArg}
 
 
-def powerk8s(*args: Sequence[Any], **kwargs: Any) -> Sequence[Mapping[str, str]]:
+def powerk8s(*_: Sequence[Any], **kwargs: Any) -> Sequence[Mapping[str, str]]:
+    """Entry point to the plugin."""
     segment_args: Mapping[SegmentArg, Any] = get_segment_args(**kwargs)
 
-    pl: PowerlineLogger = None
-    if "pl" in kwargs:
-        pl = kwargs["pl"]
+    powerline_logger: PowerlineLogger = kwargs.get("pl", None)
 
     cfg: KubeConfigLoader = None
-    with Path(KUBE_CONFIG_DEFAULT_LOCATION).expanduser().open() as f:
-        cfg = KubeConfigLoader(yaml.load(f, Loader=yaml.SafeLoader))
+    with Path(KUBE_CONFIG_DEFAULT_LOCATION).expanduser().open() as file:
+        cfg = KubeConfigLoader(yaml.load(file, Loader=yaml.SafeLoader))
 
-    if pl is not None:
-        pl.debug(f"Context: {cfg.current_context}")
-        pl.debug(f"Segment arguments: {segment_args}")
+    if powerline_logger is not None:
+        powerline_logger.debug(f"Context: {cfg.current_context}")
+        powerline_logger.debug(f"Segment arguments: {segment_args}")
 
     segments: List[SegmentData] = []
 
     if segment_args.get(SegmentArg.SHOW_KUBERNETES_LOGO, False):
-        segments.append(get_kubernetes_logo(HighlightGroup.KUBERNETES_CLUSTER))
+        segments.append(get_kubernetes_logo(HighlightGroup.KUBERNETES_CLUSTER.value))
 
     if segment_args.get(SegmentArg.SHOW_CLUSTER, False):
         segments.append(
             SegmentData(
                 contents=cfg.current_context["context"]["cluster"],
-                highlight_groups=[HighlightGroup.KUBERNETES_CLUSTER],
-                divider_highlight_group=HighlightGroup.KUBERNETES_NAMESPACE,
+                highlight_groups=[HighlightGroup.KUBERNETES_CLUSTER.value],
+                divider_highlight_group=HighlightGroup.KUBERNETES_NAMESPACE.value,
             )
         )
 
@@ -81,8 +99,8 @@ def powerk8s(*args: Sequence[Any], **kwargs: Any) -> Sequence[Mapping[str, str]]
         segments.append(
             SegmentData(
                 contents=cfg.current_context["context"]["namespace"],
-                highlight_groups=[HighlightGroup.KUBERNETES_CLUSTER],
-                divider_highlight_group=HighlightGroup.KUBERNETES_NAMESPACE,
+                highlight_groups=[HighlightGroup.KUBERNETES_CLUSTER.value],
+                divider_highlight_group=HighlightGroup.KUBERNETES_NAMESPACE.value,
             )
         )
 
--- test_powerk8s.py
+++ test_powerk8s.py
@@ -1,10 +1,9 @@
+"""Unit tests for Powerk8s."""
 from __future__ import annotations
 
 import unittest
 from pathlib import Path
 from unittest.mock import Mock, patch
-
-import kubernetes.config.kube_config  # type: ignore
 
 from powerk8s import (
     KUBERNETES_LOGO,
@@ -20,19 +19,25 @@ FILE_DIR: Path = Path(__file__).resolve().parent
 
 
 class TestGetKubernetesLogo(unittest.TestCase):
+    """Test getting the Kubernetes logo."""
+
     def test_simple(self: TestGetKubernetesLogo) -> None:
+        """Simple test case for getting the Kubernetes logo."""
         self.assertEqual(
-            get_kubernetes_logo(HighlightGroup.KUBERNETES_CLUSTER),
+            get_kubernetes_logo(HighlightGroup.KUBERNETES_CLUSTER.value),
             SegmentData(
                 KUBERNETES_LOGO,
-                [HighlightGroup.KUBERNETES_CLUSTER],
-                HighlightGroup.KUBERNETES_DIVIDER,
+                [HighlightGroup.KUBERNETES_CLUSTER.value],
+                HighlightGroup.KUBERNETES_DIVIDER.value,
             ),
         )
 
 
 class TestGetSegmentArgs(unittest.TestCase):
+    """Test getting the segment arguments."""
+
     def test_simple(self: TestGetSegmentArgs) -> None:
+        """Test getting segment args in a simple (most obvious) case."""
         self.assertEqual(
             get_segment_args(
                 show_kube_logo=True,
@@ -49,6 +54,7 @@ class TestGetSegmentArgs(unittest.TestCase):
         )
 
     def test_missing(self: TestGetSegmentArgs) -> None:
+        """Test missing segment arguments."""
         self.assertEqual(
             get_segment_args(show_cluster=True),
             {
@@ -61,7 +67,10 @@ class TestGetSegmentArgs(unittest.TestCase):
 
 
 class TestPowerK8s(unittest.TestCase):
+    """Test the Powerk8s entry point."""
+
     def setUp(self: TestPowerK8s) -> None:
+        """Set up $KUBECONFIG and mock out pathlib.Path."""
         self.kube_config_yaml = open(f"{FILE_DIR}/fixtures/dummy_kubeconfig.yaml")
 
         self.mock_path: Mock = Mock(
@@ -80,9 +89,11 @@ class TestPowerK8s(unittest.TestCase):
         )
 
     def tearDown(self: TestPowerK8s) -> None:
+        """Close the file descriptor for $KUBECONFIG."""
         self.kube_config_yaml.close()
 
     def test_simple(self: TestPowerK8s) -> None:
+        """Simple test case for powerk8s entry point."""
         with patch("powerk8s.Path", self.mock_path):
             self.assertEqual(
                 powerk8s(show_cluster=True),
@@ -96,6 +107,7 @@ class TestPowerK8s(unittest.TestCase):
             )
 
     def test_complete(self: TestPowerK8s) -> None:
+        """More extensive, table-driven case coverage for Powerk8s entry point."""
         with patch("powerk8s.Path", self.mock_path):
             self.assertEqual(
                 powerk8s(
